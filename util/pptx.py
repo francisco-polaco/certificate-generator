47dfc9a276d9fe3d1a12261594820aec7873a4e2
Francisco Santos <f.polaco.santos@gmail.com>
2025-01-31 20:08:33 +0000

    Handle groups

--- util/pptx.py
+++ util/pptx.py
@@ -1,10 +1,17 @@
-# Function to replace text in PowerPoint
+# Function to replace text in shapes
+def replace_text_in_shapes(shapes, tag, text):
+    for shape in shapes:
+        if shape.has_text_frame:
+            for para in shape.text_frame.paragraphs:
+                for run in para.runs:
+                    if tag in run.text:
+                        run.text = run.text.replace(tag, text)
+
+        elif shape.shape_type == 6:  # GroupShape -> need to recursively call this function on child shapes.
+            replace_text_in_shapes(shape.shapes, tag, text)  # Recursively process child shapes
+
+
 def replace_text_in_pptx(presentation, tag, text):
     # Replace tag in all slides
     for slide in presentation.slides:
-        for shape in slide.shapes:
-            if shape.has_text_frame:
-                for para in shape.text_frame.paragraphs:
-                    for run in para.runs:
-                        if tag in run.text:
-                            run.text = run.text.replace(tag, text)
+        replace_text_in_shapes(slide.shapes, tag, text)
